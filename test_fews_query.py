d984daea872d3569aa5bb1efdaaa83395026721f
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2019-03-01 16:48:39 +0100

    try getting CL series from FEWS dbase

--- test_fews_query.py
+++ test_fews_query.py
@@ -6,17 +6,39 @@ pi = Pi()
 pi.setClient(wsdl='http://localhost:8081/FewsPiService/fewspiservice?wsdl')
 
 tmin = Timestamp("1996")
-tmax = Timestamp("2016-11-30")
+tmax = Timestamp("2016-11-30")  # works
+# tmax = Timestamp("2016-12-01")  # fails
 
 moduleInstanceId, locationId, parameterId, _ = "ImportCAW|66004cal|P.meting.dagcal|-999.0".split("|")
 
 query = pi.setQueryParameters(prefill_defaults=True)
 query.query["onlyManualEdits"] = False
 query.parameterIds([parameterId])
-query.moduleInstanceIds([moduleInstanceId])
 query.locationIds([locationId])
 query.startTime(tmin)
 query.endTime(tmax)
-query.clientTimeZone('Europe/Amsterdam')
-
 df = pi.getTimeSeries(query, setFormat='df')
+
+#%% Test pi capabilities
+# Lijst met locaties ophalen
+locations  = pi.getLocations(setFormat="gdf")
+
+# Lijst met filters (wat dat ook mogen zijn)
+filters = pi.getFilters()
+
+# Lijst met parameters
+parameters = pi.getParameters()
+
+# Proberen CL tijdreeks op te halen?
+# Try a query for CL
+query = pi.setQueryParameters(prefill_defaults=True)
+query.query["onlyManualEdits"] = False
+query.parameterIds(["CL"])
+query.locationIds(["WIN001"])
+
+# geeft nu nog een of andere vage error over user permissions of dat er geen timeseries is
+df2 = pi.getTimeSeries(query, setFormat='df')  
+
+# Parameters ophalen voor verschillende filters:
+parameters = pi.getParameters(filterId=filters.meteo_meetstation_KNMI.id)
+parameters = pi.getParameters(filterId=filters.polder_meetpunt_actief.id)  
