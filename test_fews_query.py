ed17e773b1ffbf33d96cf7f072bbea8f7bd3b5a6
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2019-03-07 15:32:51 +0100

    add working example getting chloride timeseries

--- test_fews_query.py
+++ test_fews_query.py
@@ -3,8 +3,11 @@ from pandas import date_range, Series, DataFrame, Timestamp, Timedelta
 import numpy as np
 
 pi = Pi()
-pi.setClient(wsdl='http://localhost:8081/FewsPiService/fewspiservice?wsdl')
+# pi.setClient(wsdl='http://localhost:8081/FewsPiService/fewspiservice?wsdl')  # 2017.01
+pi.setClient(wsdl='http://localhost:8080/FewsWebServices/fewspiservice?wsdl')  # 2017.02
 
+
+# Test ophalen van neerslag reeks.
 tmin = Timestamp("1996")
 tmax = Timestamp("2016-11-30")  # works
 # tmax = Timestamp("2016-12-01")  # fails
@@ -17,35 +20,38 @@ query.parameterIds([parameterId])
 query.locationIds([locationId])
 query.startTime(tmin)
 query.endTime(tmax)
+query.version("1.24")
 df = pi.getTimeSeries(query, setFormat='df')
 
-# Lijst met filters (wat dat ook mogen zijn)
-filters = pi.getFilters()
+# Proberen CL tijdreeks op te halen? Try a query for CL:
+tmin = Timestamp("2000-01-01 00:00")
+tmax = Timestamp("2017-01-01 00:00")
+query = pi.setQueryParameters(prefill_defaults=True)
+query.query["onlyManualEdits"] = False
+query.parameterIds(["CL"])
+query.locationIds(["WIN001"])
+query.startTime(tmin)
+query.endTime(tmax)
+query.version("1.24")
+query.forecastSearchCount(1)
+query.useDisplayUnits(False)
+query.showThresholds(True)
+query.omitMissing(True)
 
-ts = pi.getTimeSeriesForFilter2(filterId=filters.meteo_meetstation_KNMI.id,
-                                locationIds=np.array([locationId], dtype=str), 
-                                parameterIds=np.array([parameterId], dtype=str), 
-                                startTime=tmin,
-                                endTime=tmax, 
-                                setFormat="df")
+# Dit werkt nu!
+df2 = pi.getTimeSeries(query, setFormat='df')  
 
 #%% Test pi capabilities
+
 # Lijst met locaties ophalen
 locations  = pi.getLocations(setFormat="gdf")
 
 # Lijst met parameters
 parameters = pi.getParameters()
 
-# Proberen CL tijdreeks op te halen?
-# Try a query for CL
-query = pi.setQueryParameters(prefill_defaults=True)
-query.query["onlyManualEdits"] = False
-query.parameterIds(["CL"])
-query.locationIds(["WIN001"])
-
-# geeft nu nog een of andere vage error over user permissions of dat er geen timeseries is
-df2 = pi.getTimeSeries(query, setFormat='df')  
+# Lijst met filters
+filters = pi.getFilters()
 
 # Parameters ophalen voor verschillende filters:
-parameters = pi.getParameters(filterId=filters.meteo_meetstation_KNMI.id)
-parameters = pi.getParameters(filterId=filters.polder_meetpunt_actief.id)  
+parameters = pi.getParameters(filterId=filters.PI_service_intern.id)
+parameters = pi.getParameters(filterId=filters.polder_meetpunt_actief.id)
